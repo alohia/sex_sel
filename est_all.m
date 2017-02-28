1da093026c782464ab1488a4f6fdc22d29dcc8b4
Akhil Lohia <akhil.lohia@gmail.com>
2017-02-28 10:00:18 +0100

    fix est_all

--- est_all.m
+++ est_all.m
@@ -3,10 +3,12 @@ function [ SSR ] = est_all( castes,params )
 %   This function solves the model for the given parameters for each caste
 %   and gives the sum of squared errors across all castes
 
-num_castes = numel(castes(:,1));
+%num_castes = numel(castes(:,1));
+num_castes = numel(castes);
 SSR = 0;
 for i = 1:num_castes
-    [X, Hky ,n] = getdata(castes(i,1));
+    [X, Hky ,n] = getdata(castes(i));
+    %[X, Hky ,n] = getdata(castes(i,1));
     X(:,1) = X(:,1)*10;
     Y = X;
     SSR = SSR + sumsqr(Hky - solve_model(X,Y,params));
