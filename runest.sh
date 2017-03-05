29dff3a6347e608c72c8647ac72532b0bf6883f6
Akhil Lohia <alohia@sitmaster.s.upf.edu>
2017-03-05 15:23:04 +0100

    bootstrap prep

--- runest.sh
+++ runest.sh
@@ -3,13 +3,12 @@
 #$ -M akhil.lohia@upf.edu
 #$ -m abe
 #$ -r n
-#$ -o $HOME/out.txt
-#$ -e $HOME/error.txt
-#$ -pe threaded 4
-#$ -l h_vmem=5.5G
-#$ -l h_rt=02:00:00
+# -o $HOME/out.txt
+# -e $HOME/error.txt
+# -pe threaded 4
+# -l h_vmem=6G
 #$ -cwd
 
 module load matlab
 
-matlab -nodisplay -logfile log.log -r estimation
+matlab -nodisplay -logfile log.log -r "castes = [$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11]; disp(castes)"
---
+++ test.sh
@@ -0,0 +1,1 @@
+echo job1; castes=(1 5 7 10 14 17 23 28 33 38 44);qsub runest.sh ${castes[@]}
