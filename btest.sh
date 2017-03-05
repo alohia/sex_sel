38d66f206eb572697edc11c4505ed64c4e5739fa
Akhil Lohia <akhil.lohia@gmail.com>
2017-03-05 16:10:06 +0100

    fix scripts

--- btest.sh
+++ btest.sh
@@ -1,2 +1,2 @@
-echo job1; castes=[28 28 38 17 33 28 33 38 14 10  1  1];qsub runest.sh ${castes[@]}
-echo job2; castes=[23  1 28 28 38 38 28 14 17 28 23  2];qsub runest.sh ${castes[@]}
+echo job1; castes=(38 14 28 28 28 28 28 38 1 23 28 1);qsub runest.sh ${castes[@]}
+echo job2; castes=(28 28 38 28 38 7 28 28 38 44 28 2);qsub runest.sh ${castes[@]}
