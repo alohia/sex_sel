05efcc9446fe02c79ac707cef7e59ce6a6ae3b65
Akhil Lohia <alohia@sitmaster.s.upf.edu>
2017-03-12 22:25:09 +0100

    test bs

--- runest.sh
+++ runest.sh
@@ -6,9 +6,9 @@
 # -o $HOME/out.txt
 # -e $HOME/error.txt
 # -pe threaded 3
-# -l h_vmem=8G
+#$ -l h_vmem=8G
 #$ -cwd
 
 module load matlab
 
-matlab -nodisplay -nojvm -logfile log.log -r "castes = [$1, $2, $3, $4, $5, $6, $7, $8, $9, ${10}, ${11}, ${12}]; bstrap"
+matlab -nodisplay -nojvm -logfile log.log -r "castes = [$1, $2, $3, $4, $5, $6, $7, $8, $9, ${10}, ${11}, ${12} ${13}]; estimation"
